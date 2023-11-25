64dd4decda7c11c04bd183744a0dd2faf745643d
髙橋紀美 <xvipea0@gmail.com>
2023-11-25 14:32:50 +0900

    When the screen is displayed, fetch the items registered in the TODO list from CoreData.
    画面が表示された時 CoreDataからTODOリストに登録されている項目を取得する.

--- CoreDataSumpleApp/ViewController.swift
+++ CoreDataSumpleApp/ViewController.swift
@@ -18,9 +18,27 @@ class TasksViewController: UITableViewController {
         return appDelegate.persistentContainer.viewContext
     }
     
+    // 画面が表示された時 CoreDataからTODOリストに登録されている項目を取得する。
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        let context = getContext()
+        // fetchRequestはCore DataからTaskエンティティのインスタンスを取得するためのリクエストを作成し、実行する役割を担っている。
+        // ジェネリクスを使うのは定石。これにより、フェッチリクエストの結果がそのエンティティタイプの配列であることがコンパイル時に保証できる。
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        // sortDescriptionはNSSortDescriptorのインスタンスであり、フェッチリクエストの結果を特定の順序でソートするために使用される。
+        // ソートキーの指定 = `key: "title"` , ソート順の指定(降順) = `ascending: false`
+        let sortDescription = NSSortDescriptor(key: "title", ascending: false)
         
+        // fetchRequestにsortDescriptorsプロパティとしてこのsortDescriptionを設定することで、
+        // フェッチされるTaskのリストがtitle属性に基づいて降順にソートされた状態で取得される。
+        fetchRequest.sortDescriptors = [sortDescription]
+        
+        // contextを使用して、fetchRequestに基づいてデータを取得し、それをtasks配列に格納する
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
     
     override func viewDidLoad() {
